facf0017026acf86a112192bfd0aa0d84cb72e41
martinodonnell <modo1997@hotmail.co.uk>
2020-01-26 17:46:39 +0000

    Set up testing function

--- datasets.py
+++ datasets.py
@@ -19,6 +19,18 @@ def load_class_names(path='data/StanfordCars/devkit/cars_meta.mat'):
     cn = [str(c[0].item()) for c in cn]
     return cn
 
+def load_boxcar_class_names():
+    ann = []
+    with open(BOXCARS_HARD_CLASS_NAMES, 'r') as filehandle:
+            for line in filehandle:
+                # remove linebreak which is the last character of the string
+                currentPlace = line[:-1]
+
+                # add item to the list
+                ann.append(currentPlace)
+
+    return ann
+
 def load_annotations(path):
     ann = load_anno(path)['annotations'][0]
     ret = {}
@@ -46,9 +58,6 @@ class CarsDataset(Dataset):
         elif(dataset==2):
             boxCarsAnnUtil = BoxCarDataset(imgdir, anno_path, transform, size,dataset,split,part)
             self.annos  = boxCarsAnnUtil.load_annotations_boxcars()
-            # with open('data/' + boxCarsAnnUtil.current_part , 'w') as f:
-            #     for item in boxCarsAnnUtil.cars_annotations:
-            #         f.write("%s\n" % item)
         else:
             print("No dataset. Leaving")
             exit(1)
@@ -220,5 +229,40 @@ def prepare_loader(config):
 
     return train_loader, test_loader
 
+
+def prepare_test_loader(config):
+    if(config['dataset']==1):
+        print("NOT SET UP YET")
+        exit(1)
+        train_imgdir = STANFORD_CARS_TRAIN
+        test_imgdir = STANFORD_CARS_TEST
+
+        train_annopath = STANFORD_CARS_TRAIN_ANNOS
+        test_annopath = STANFORD_CARS_TEST_ANNOS
+
+    elif(config['dataset']==2):
+        test_imgdir = test_imgdir =  BOXCARS_IMAGES_IMAGES
+        test_annopath = test_annopath = BOXCARS_DATASET_ROOT
+    else:
+        print("No dataset. Leaving")
+        exit(1)
+
+    test_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4706145, 0.46000465, 0.45479808), (0.26668432, 0.26578658, 0.2706199))
+        ]
+    )
+
+    
+    test_dataset = CarsDataset(test_imgdir, test_annopath, test_transform, config['imgsize'],config['dataset'],config['split'],'test')
+
+    test_loader = DataLoader(test_dataset,
+                             batch_size=config['test_batch_size'],
+                             shuffle=False,
+                             pin_memory=False,
+                             num_workers=12)
+
+    return test_loader
     
 
---
+++ test.py
@@ -0,0 +1,187 @@
+import argparse
+import json
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import time
+
+from datasets import load_boxcar_class_names, prepare_test_loader
+from models import construct_model
+
+
+def test_v1(model, test_loader, device, config):
+    model.eval()
+
+    loss_meter = 0
+    acc_meter = 0
+    runcount = 0
+    elapsed = 0
+    i = 0
+
+    with torch.no_grad():
+        start_time = time.time()
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+
+            pred = model(data)
+
+            loss = F.cross_entropy(pred, target) * data.size(0)
+            acc = pred.max(1)[1].eq(target).float().sum()
+
+            loss_meter += loss.item()
+            acc_meter += acc.item()
+            i += 1
+            elapsed = time.time() - start_time
+            runcount += data.size(0)
+
+            print(f'[{i}/{len(test_loader)}]: '
+                  f'Loss: {loss_meter / runcount:.4f} '
+                  f'Acc: {acc_meter / runcount:.4f} ({elapsed:.2f}s)', end='\r')
+
+        print()
+
+        loss_meter /= runcount
+        acc_meter /= runcount
+
+    valres = {
+        'val_loss': loss_meter,
+        'val_acc': acc_meter,
+        'val_time': elapsed
+    }
+
+    print(f'Test Result: Loss: {loss_meter:.4f} Acc: {acc_meter:.4f} ({elapsed:.2f}s)')
+
+    return valres
+
+
+def test_v2(model, test_loader, device, config):
+    model.eval()
+
+    loss_meter = 0
+    acc_meter = 0
+    make_acc_meter = 0
+    type_acc_meter = 0
+    runcount = 0
+
+    i = 0
+
+    with torch.no_grad():
+        start_time = time.time()
+        for data, target, make_target, type_target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+            make_target = make_target.to(device)
+            type_target = type_target.to(device)
+
+            pred, make_pred, type_pred = model(data)
+
+            loss_main = F.cross_entropy(pred, target)
+            loss_make = F.cross_entropy(make_pred, make_target)
+            loss_type = F.cross_entropy(type_pred, type_target)
+
+            loss = loss_main + config['make_loss'] * loss_make + config['type_loss'] * loss_type
+
+            acc = pred.max(1)[1].eq(target).float().sum()
+            make_acc = make_pred.max(1)[1].eq(make_target).float().sum()
+            type_acc = type_pred.max(1)[1].eq(type_target).float().sum()
+
+            loss_meter += loss.item() * data.size(0)
+            acc_meter += acc.item()
+            make_acc_meter += make_acc.item()
+            type_acc_meter += type_acc.item()
+
+            runcount += data.size(0)
+            i += 1
+            elapsed = time.time() - start_time
+
+            print(f'[{i}/{len(test_loader)}]: '
+                  f'Loss: {loss_meter / runcount:.4f} '
+                  f'Acc: {acc_meter / runcount:.4f} '
+                  f'Make: {make_acc_meter / runcount:.4f} '
+                  f'Type: {type_acc_meter / runcount:.4f} '
+                  f'({elapsed:.2f}s)', end='\r')
+
+        print()
+
+        elapsed = time.time() - start_time
+
+        loss_meter /= runcount
+        acc_meter /= runcount
+        make_acc_meter /= runcount
+        type_acc_meter /= runcount
+
+    print(f'Test Result: Loss: {loss_meter:.4f} Acc: {acc_meter:.4f} ({elapsed:.2f}s)')
+
+    valres = {
+        'val_loss': loss_meter,
+        'val_acc': acc_meter,
+        'val_make_acc': make_acc_meter,
+        'val_type_acc': type_acc_meter,
+        'val_time': elapsed
+    }
+
+    return valres
+
+
+def load_weight(model, path, device):
+    sd = torch.load(path,map_location=device)
+    model.load_state_dict(sd)
+
+
+def main(args):
+    device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
+
+    config = {
+        'batch_size': 32,
+        'test_batch_size': 32,
+        'lr': 0.01,
+        'weight_decay': 0.0001,
+        'momentum': 0.9,
+        'epochs': 60,
+        'imgsize': (224, 244),
+        # 'arch': args.arch,
+        # 'version': args.version,
+        # 'make_loss': args.make_loss,
+        # 'type_loss': args.type_loss,
+        'finetune': False,
+        'dataset':2,
+        'split':'hard',
+        # 'path': args.path
+    }
+    # config = json.load(open(args.config))
+    # config['imgsize'] = (args.imgsize, args.imgsize)
+    # exp_dir = os.path.dirname(args.config)
+    # modelpath = exp_dir + '/best.pth'
+
+    modelpath = args.modelPath
+
+    class_names = load_boxcar_class_names()
+    num_classes = len(class_names)
+    # v2_info = separate_class(class_names)
+    # num_makes = len(v2_info['make'].unique())
+    # num_types = len(v2_info['model_type'].unique())
+
+    model = construct_model(config, num_classes)
+    load_weight(model, modelpath, device)
+    model = model.to(device)
+
+    test_loader = prepare_test_loader(config)
+
+    test_fn = test_v1
+
+    test_fn(model, test_loader, device, config)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Testing script for Cars dataset')
+
+    parser.add_argument('--modelPath', required=True,
+                        help='path to modelPath')
+
+    args = parser.parse_args()
+
+    main(args)
